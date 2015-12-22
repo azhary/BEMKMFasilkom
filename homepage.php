d64571699c1eaac40660cec3d4993306dafc1001
Azhary Arliansyah <arliansyah_azhary@yahoo.com>
2015-12-23 01:03:15 +0700

    testing bae

---
+++ homepage.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BEM KM Fasilkom Unsri</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<script type="text/javascript" src="js/jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+		        </button>
+				<a class="navbar-brand" href="#">BEM KM Fasilkom Unsri</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav navbar-right">
+		      	<li><a href="#deskripsi">Visi-Misi</a></li>
+		      	<li class="dropdown">
+		          <a href="#">Biro dan Departemen <span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="info.php#humas">Biro Humas</a></li>
+		            <li><a href="info.php#kastrad">Biro Kastrad</a></li>
+		            <li><a href="info.php#kestari">Biro Kestari</a></li>
+		            <li><a href="info.php#media">Biro Media</a></li>
+		            <li><a href="info.php#psdm">Biro PSDM</a></li>
+		            <li><a href="info.php#pti">Biro PTI</a></li>
+		            <li class="divider"></li>
+		            <li><a href="info.php#keilmuan">Departemen Keilmuan</a></li>
+		            <li><a href="info.php#kewirausahaan">Departemen Kewirausahaan</a></li>
+		            <li><a href="info.php#pengsos">Departemen Pengabdian Sosial</a></li>
+		            <li><a href="info.php#senbud">Departemen Seni dan Budaya</a></li>
+		          </ul>
+		        </li>
+		      	<li><a href="#">Karya Anak Ilkom</a></li>
+		        <li><a href="#">Info Beasiswa</a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+	<div id="slide-show" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#slide-show" data-slide-to="0" class="active"></li>
+	    <li data-target="#slide-show" data-slide-to="1"></li>
+	  </ol>
+
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+	      <img src="img/fasilkom.png" alt="Fasilkom">
+	      <div class="carousel-caption">
+	        <p class="lead">Universitas Sriwijaya</p>
+	      </div>
+	    </div>
+	    <div class="item">
+	      <img src="img/dekanat.jpg" alt="Gedung Dekanat">
+	      <div class="carousel-caption">
+	        <p class="lead">Gedung Dekanat</p>
+	      </div>
+	    </div>
+	    
+	  </div>
+
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#slide-show" role="button" data-slide="prev">
+	    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#slide-show" role="button" data-slide="next">
+	    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+	<div id="deskripsi">
+		<div class="container section">
+			<h1>Deskripsi</h1>
+			<p class="lead">
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+			</p>
+		</div>
+	</div>
+	<hr/>
+	<div class="container" id="news">
+		<div class="row">
+			<div class="col-md-4">
+				<h1>News!</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<a href="#" class="btn btn-primary">Read More >></a>
+			</div>
+			<div class="col-md-4">
+				<h1>News!</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<a href="#" class="btn btn-primary">Read More >></a>
+			</div>
+			<div class="col-md-4">
+				<h1>News!</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<a href="#" class="btn btn-primary">Read More >></a>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h1>Sekretariat</h1>
+					<p>Gedung Fakultas Ilmu Komputer Kampus Unsri Indralaya km 33</p>
+				</div>
+				<div class="col-md-offset-8">
+					<h1>Media Sosial</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript">
+		$(".section").css("min-height", $(window).height());
+	</script>
+</body>
+</html>
---
+++ info.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BEM KM Fasilkom Unsri</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<script type="text/javascript" src="js/jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+		        </button>
+				<a class="navbar-brand" href="homepage.php">BEM KM Fasilkom Unsri</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav navbar-right">
+		      	<li><a href="#deskripsi">Visi-Misi</a></li>
+		      	<li class="dropdown">
+		          <a href="#">Biro dan Departemen <span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="info.php#humas">Biro Humas</a></li>
+		            <li><a href="info.php#kastrad">Biro Kastrad</a></li>
+		            <li><a href="info.php#kestari">Biro Kestari</a></li>
+		            <li><a href="info.php#media">Biro Media</a></li>
+		            <li><a href="info.php#psdm">Biro PSDM</a></li>
+		            <li><a href="info.php#pti">Biro PTI</a></li>
+		            <li class="divider"></li>
+		            <li><a href="info.php#keilmuan">Departemen Keilmuan</a></li>
+		            <li><a href="info.php#kewirausahaan">Departemen Kewirausahaan</a></li>
+		            <li><a href="info.php#pengsos">Departemen Pengabdian Sosial</a></li>
+		            <li><a href="info.php#senbud">Departemen Seni dan Budaya</a></li>
+		          </ul>
+		        </li>
+		      	<li><a href="#">Karya Anak Ilkom</a></li>
+		        <li><a href="#">Info Beasiswa</a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+	<div id="info">
+		<div class="row section" id="title">
+			<div class="container">
+				<h1>OPEN RECRUITMENT</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+		<div class="row" id="ops">
+			<div class="container">
+				<div class="row" id="humas">
+					<h1>Biro Humas</h1>	
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+				<div class="row" id="kastrad">
+					<h1>Biro Kastrad</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>	
+				</div>
+				<div class="row" id="kestari">
+					<h1>Biro Kestari</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>	
+				</div>
+				<div class="row" id="media">
+					<h1>Biro Media</h1>	
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+				<div class="row" id="psdm">
+					<h1>Biro PSDM</h1>	
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+				<div class="row" id="pti">
+					<h1>Biro PTI</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>	
+				</div>
+				<div class="row" id="keilmuan">
+					<h1>Departemen Keilmuan</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>	
+				</div>
+				<div class="row" id="kewirausahaan">
+					<h1>Departemen Kewirausahaan</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>	
+				</div>
+				<div class="row" id="pengsos">
+					<h1>Departemen Pengabdian Sosial</h1>	
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+				<div class="row" id="senbud">
+					<h1>Departemen Seni dan Budaya</h1>	
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="row section" id="tgl">
+			<div class="container">
+				<h1>Tanggal dan Tempat</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<button class="btn btn-success btn-lg" data-toggle="modal" data-target="#form-pendaftaran">Daftar</button>
+				<div class="modal fade" id="form-pendaftaran" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				  <div class="modal-dialog" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				        <h4 class="modal-title" id="myModalLabel">Formulir Pendaftaran</h4>
+				      </div>
+				      <div class="modal-body">
+				        ...
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				        <button type="button" class="btn btn-primary">Save changes</button>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h1>Sekretariat</h1>
+					<p>Gedung Fakultas Ilmu Komputer Kampus Unsri Indralaya km 33</p>
+				</div>
+				<div class="col-md-offset-8">
+					<h1>Media Sosial</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript">
+		$(".section").css("min-height", $(window).height());
+	</script>
+</body>
+</html>
