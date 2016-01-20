6214e4892fb6b37c112ce51a3d732c00a54a058f
azhary <engineeringwithlove@gmail.com>
2016-01-20 21:05:50 +0700

    test

---
+++ index.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Open Recruitment | BEM KM Fasilkom Unsri</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="img/logo.png">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <style>
+	  body {
+	      position: relative;
+	  }
+	  .affix {
+	      top: 20px;
+	  }
+	  div.col-sm-9 div {
+	      height: 250px;
+	      font-size: 20px;
+	  }
+
+	  h1 {
+	  	padding-top: 5%;
+	  }
+
+	  #sections {
+	  	border-left: 1px solid #555;
+	  	background-color: #7f8c8d;
+	  }
+
+	  #pamflet {
+	  	width: 100%;
+	  }
+
+	  #humas {
+	  	background-color: #2ecc71;
+	  	padding: 5%;
+	  }
+
+	  #kastrad {
+	  	background-color: #e74c3c;
+	  	padding: 5%;
+	  }
+
+	  #kestari {
+	  	background-color: #f1c40f;
+	  	padding: 5%;
+	  }
+
+	  #keilmuan {
+	  	background-color: #9b59b6;
+	  	padding: 5%;
+	  }
+
+	  #kewirausahaan {
+	  	background-color: #e67e22;
+	  	padding: 5%;
+	  }
+
+	  #multimedia {
+	  	background-color: #1abc9c;
+	  	padding: 5%;
+	  }
+
+	  #olahraga {
+	  	background-color: #FF6961;
+	  	padding: 5%;
+	  }
+
+	  #pengsos {
+	  	background-color: #AEC6CF;
+	  	padding: 5%;
+	  }
+
+	  #pti {
+	  	background-color: #3498db;
+	  	padding: 5%;
+	  }
+
+	  #psdm {
+	  	background-color: #FDFD96;
+	  	padding: 5%;
+	  }
+
+	  #senbud {
+	  	background-color: #CB99C9;
+	  	padding: 5%;
+	  }
+
+	  #daftar {
+	  	text-align: center;
+	  	background-color: #bdc3c7;
+	  }
+
+	  #btndaftar {
+	  	width: 200px;
+	  	height: 60px;
+	  	font-size: 32px;
+	  }
+
+	  #copyright {
+	  	margin-top: 12%;
+	  }
+
+	  #copyright, #license {
+	  	font-size: 12px;
+	  }
+
+	  hr {
+	  	border-top: 1px solid #888;
+	  	margin: 0;
+	  	margin-right: -40px;  
+	  }
+
+	  .img-responsive {
+	  	margin-left: 25%;
+	  }
+	  
+	  @media screen and (max-width: 810px) {
+	    #section1, #section2, #section3, #section41, #section42  {
+	        margin-left: 150px;
+
+	    }
+	    div.col-sm-9 div {
+	      height: 250px;
+	      font-size: 14px;
+	  	}
+	  	.img-responsive {
+	  		margin-left: 35%;
+	  	}
+
+	  	#pamflet {
+	  		height: 100%;
+	  	}
+	  }
+  </style>
+</head>
+<body data-spy="scroll" data-target="#scrollspy" data-offset="15">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+	<div >
+	  <div class="row">
+	    <nav class="col-sm-3" id="scrollspy">
+	      <ul class="nav nav-pills nav-stacked text-center" data-spy="affix" >
+	        <li><a href="#persyaratan">Persyaratan</a></li>
+	        <hr/>
+	        <li><a href="#humas">Hubungan Masyarakat</a></li>
+	        <li><a href="#kastrad">Kajian Strategi dan Advokasi</a></li>
+	        <li><a href="#kestari">Kesekretariatan</a></li>
+	        <li><a href="#keilmuan">Keilmuan</a></li>
+	        <li><a href="#kewirausahaan">Kewirausahaan</a></li>
+	        <li><a href="#multimedia">Multimedia</a></li>
+	        <li><a href="#olahraga">Olahraga</a></li>
+	        <li><a href="#pengsos">Pengabdian Sosial</a></li>
+	        <li><a href="#psdm">Pengembangan Sumber Daya Mahasiswa</a></li>
+	        <li><a href="#pti">Pengembangan Teknologi Informasi</a></li>
+	        <li><a href="#senbud">Seni dan Budaya</a></li>
+	        <hr/>
+	        <li><a href="#daftar">DAFTAR DISINI</a></li>
+	        <hr/>
+	        <li id="copyright">&copy BEM KM Fasilkom Unsri 2016</li>
+	        <li id="license">Pengembangan Teknologi Informasi</li>
+	      </ul>
+	    </nav>
+	    <div class="col-sm-9" id="sections">
+	      <div class="pamflet row" id="persyaratan">    
+	        <img id="pamflet" src="img/pamflet.png">
+	      </div>        
+	      <div class="section row" id="humas">         
+	        <h1 class="text-center">Hubungan Masyarakat</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/humas.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Dinas Humas adalah salah satu Dinas yang ada di BEM KM Fasilkom Unsri yang bertujuan membangun hubungan antara sesama Mahasiswa Fasilkom maupun pihak luar untuk keberlangsungan BEM KM Fasilkom Unsri. Demi tercapainya Visi dan Misi BEM KM Fasilkom Unsri dengan baik.
+	        	</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="kastrad">         
+	        <h1 class="text-center">Kajian Strategi dan Advokasi</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/kastrad.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Dinas Kajian Strategi dan Advokasi adalah salah satu Dinas yang terdapat di BEM KM Fasilkom Unsri yang bertujuan untuk menampung Aspirasi Mahasiswa Fasilkom dan merupakan jembatan penyalur aspirasi mahasiswa ke pihak dekanat dan dibantu oleh BEM KM Fasilkom Unsri itu sendiri. Dinas Kastrad dibentuk bertujuan agar dapat tersalurnya aspirasi mahasiswa ke pihak dekanat sehingga dapat diproses lebih lanjut lagi.
+	
+	        	</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="kestari">         
+	        <h1 class="text-center">Kesekretariatan</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/kestari.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Dinas ini memiliki dua fungsi utama yaitu Fungsi Administrasi dan Fungsi Kerumahtanggaan. Fungsi Administrasi artinya Kestari bertanggung jawab untuk setiap Proposal, SPJ, serta LPJ program kerja BEM KM Fasilkom unsri serta urusan surat menyurat lainnya. Lalu Fungsi Kerumahtanggaan meliputi pengadaan Inventaris dan Perawatan sekretariat BEM KM Fasilkom Unsri.
+
+				</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="keilmuan">         
+	        <h1 class="text-center">Keilmuan</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/keilmuan.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Memberikan sebuah layanan keilmuan bagi warga Fakultas Ilmu Komputer Universitas Sriwijaya dan membentuk ruang publik yang mengakomodir serta menciptakan hubungan baik antar elemen keilmuan di Fakultas Ilmu Komputer Universitas Sriwijaya dan di luar Fakultas Ilmu Komputer Universitas Sriwijaya.
+
+				</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="kewirausahaan">         
+	        <h1 class="text-center">Kewirausahaan</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/kewirausahaan.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Dinas kewirausahaan adalah kemampuan untuk menciptakan sesuatu yang baru dan berbeda. Menghimpun orang-orang yang berwirausaha khususnya di Fakultas Ilmu Komputer Universitas Sriwijaya, di buat pelatihan Dinas Kewirausahaan juga mencari pemasukan untuk kas BEM KM Fasilkom Unsri, dan di juga bergerak di bidang wirausaha untuk mendukung orang-orang yang berminat. di bidang tersebut. mencari dana melalui wirausaha atau sponsorship.
+
+				</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="multimedia">         
+	        <h1 class="text-center">Multimedia</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/media.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Dinas Multimedia adalah Dinas yang dibentuk sebagai penyedia Informasi agar mudah diakses Mahasiswa Fakultas Ilmu Komputer Universitas Sriwijaya serta media publikasi dan membuat Desain Grafis dalam kegiatan BEM KM Fasilkom Unsri. Maka dari itu Dinas multimedia memiliki slogan yang sesuai dengan fungsi Dinas media.
+
+				</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="olahraga">         
+	        <h1 class="text-center">Olahraga</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/olahraga.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Bidang Minat Bakat Dinas Olahraga yang ada di BEM KM Fasilkom Unsri yang bertujuan untuk mengembangkan minat dan bakat mahasiswa di bidang olahraga di Faskultas Ilmu Komputer Universitas Sriwijaya. Untuk tercapainya visi dan misi BEM KM Fasilkom Unsri dengan baik, maka Dinas Olahraga mempunyai slogan yang sesuai dengan bakat mahasiswa.
+
+				</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="pengsos">         
+	        <h1 class="text-center">Pengabdian Sosial</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/pengsos.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Dinas yang bergerak di bidang eksternal, ini adalah salah satu perpanjangan tangan dari mahasiswa yang harapannya bisa mensinergikan mahasiswa untuk semakin dekat dengan masyarakat, untuk semakin memahami bahwa salah satu tujuan kita kuliah adalah untuk mencari ilmu dan mengaplikasikan ilmu yang telah kita dapat kepada masyarakat. Selain perpajangan tangan, Dinas ini juga memiliki peran untuk memberdayakan masyarakat, dan untuk mewujudkannya tentu saja kita membutuhkan sinergi dengan mahasiswa secara umum.	
+	        	</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="psdm">         
+	        <h1 class="text-center">Pengembangan Sumber Daya Mahasiswa</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/psdm.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Dinas ini menjadi supporting system untuk mengembangkan sumber daya mahasiswa yang ada di BEM KM Fasilkom Unsri. Soft skill maupun hard skill BEM KM Fasilkom Unsri, dengan kompetensi dasar kaderisasi, upgrading, dan management development. Dinas ini juga menjaga keutuhan, kekeluargaan serta kekompakan dari seluruh anggota BEM KM Fasilkom Unsri.
+
+				</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="pti">         
+	        <h1 class="text-center">Pengembangan Teknologi Informasi</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/pti.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Dinas Pengembangan Teknologi Informasi(PTI) adalah salah satu Dinas BEM KM Fasilkom Unsri yang bertugas menunjang kinerja BEM KM Fasilkom Unsri agar dapat dilaksanakan secara efektif dan efisien dengan mengembangkan Teknologi Informasi melalui proses <i>Software Development</i> dan <i>IT Supporting</i>. Kita mempunyai slogan yang sesuai dengan filosofi algoritma bahwa algoritma yang baik yaitu algoritma yang dapat menyelesaikan masalah dengan menggunakan <i>resource</i> sesedikit mungkin dan waktu sesingkat mungkin. 
+
+				</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="senbud">         
+	        <h1 class="text-center">Seni dan Budaya</h1>
+	        <div class="row">
+	        	<div class="col-md-3">
+	        		<img src="img/senbud.png" class="img-responsive">
+	        	</div>
+	        	<div class="col-md-9">
+	        		Dinas Seni dan Budaya adalah salah satu pengembangan minat dan bakat Mahasiswa Fakultas Ilmu Komputer Universitas Sriwijaya yang berdiri di dalam BEM KM Fasilkom Unsri. Dinas Seni dan Budaya ini di ciptakan untuk Mahasiswa Fakultas Ilmu Komputer Universitas Sriwijaya yang memiliki bakat dalam seni tari, seni rupa, vokal dan 
+kemampuan bermain musik. Maka dari itu Dinas Seni dan Budaya memiliki slogan yang sesuai dengan budaya bangsa ini.
+
+	        	</div>
+	        </div>
+	      </div>
+	      <div class="section row" id="daftar">         
+	        <h1 class="text-center">Klik tombol di bawah ini untuk mendaftar</h1>
+	        <a href="http://bem.ilkom.unsri.ac.id/or"><button id="btndaftar" class="btn btn-success">Daftar</button></a>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<script type="text/javascript">
+		$(".section").css("height", $(window).height() * 0.8);
+		$(".pamflet").css("height", $(window).height() * 2.1)
+		$("#scrollspy a").on('click', function(event){
+
+		 
+		  event.preventDefault();
+
+		  
+		  var hash = this.hash;
+
+		  
+		  $('html, body').animate({
+		    scrollTop: $(hash).offset().top
+		  }, 800, function(){
+
+		    
+		    window.location.hash = hash;
+		  });
+		});
+
+		
+		if ($(window).width() <= 830){	
+			$(".section").css("height", $(window).height() * 1.2);
+		}	
+		
+	</script>
+</body>
+</html>                                		
